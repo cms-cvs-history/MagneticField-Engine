fc6ed03e06e51780dc7596737ea104e6cbff23d4
Nicola Amapane <sha1-ecfc50bc11229bbd7ce8ea817e47f97cd3162da5@cern.ch>
2008-11-04 09:14:40 +0000

    Convert to python

---
+++ test/queryField_cfg.py
@@ -0,0 +1,44 @@
+#
+
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("MAGNETICFIELDTEST")
+process.source = cms.Source("EmptySource")
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(1)
+)
+
+# Uniform field
+#process.load("Configuration.StandardSequences.MagneticField_0T_cff")
+#process.localUniform.ZFieldInTesla = 3.8
+
+# Full field map, uncomment one
+#process.load("Configuration.StandardSequences.MagneticField_20T_cff")
+
+#process.load("Configuration.StandardSequences.MagneticField_30T_cff")
+
+#process.load("Configuration.StandardSequences.MagneticField_35T_cff")
+
+process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+
+#process.load("Configuration.StandardSequences.MagneticField_40T_cff")
+
+
+process.MessageLogger = cms.Service("MessageLogger",
+    categories   = cms.untracked.vstring("MagneticField"),
+    destinations = cms.untracked.vstring("cout"),
+    cout = cms.untracked.PSet(  
+    noLineBreaks = cms.untracked.bool(True),
+    threshold = cms.untracked.string("WARNING"),
+    WARNING = cms.untracked.PSet(
+      limit = cms.untracked.int32(0)
+    ),
+    MagneticField = cms.untracked.PSet(
+     limit = cms.untracked.int32(10000000)
+    )
+  )
+)
+
+process.queryField  = cms.EDProducer("queryField")
+process.p1 = cms.Path(process.queryField)
+
