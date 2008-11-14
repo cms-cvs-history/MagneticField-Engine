9d7c0c1bdd5123baea13e5c790704bcf53dfb284
Nicola Amapane <sha1-ecfc50bc11229bbd7ce8ea817e47f97cd3162da5@cern.ch>
2008-11-14 11:14:37 +0000

    Update test

--- test/queryField_cfg.py
+++ test/queryField_cfg.py
@@ -8,21 +8,31 @@ process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(1)
 )
 
+
+# Example configuration for the magnetic field
+
+
+# Uncomment ONE of the following:
+
+
 # Uniform field
 #process.load("Configuration.StandardSequences.MagneticField_0T_cff")
 #process.localUniform.ZFieldInTesla = 3.8
 
-# Full field map, uncomment one
+# Full field map, static configuration
 #process.load("Configuration.StandardSequences.MagneticField_20T_cff")
 
 #process.load("Configuration.StandardSequences.MagneticField_30T_cff")
 
 #process.load("Configuration.StandardSequences.MagneticField_35T_cff")
 
-process.load("Configuration.StandardSequences.MagneticField_38T_cff")
+#process.load("Configuration.StandardSequences.MagneticField_38T_cff")
 
 #process.load("Configuration.StandardSequences.MagneticField_40T_cff")
 
+# Automatically select map based on recorded energy in the DB
+process.load("MagneticField.Engine.autoMagneticFieldProducer_cfi")
+process.AutoMagneticFieldESProducer.value = 38
 
 process.MessageLogger = cms.Service("MessageLogger",
     categories   = cms.untracked.vstring("MagneticField"),
